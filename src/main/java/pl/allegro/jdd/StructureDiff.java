e60ccbd83a2c5fb2c64d969b3a536f980be818a9
Daniel Stankiewicz <dstankiew@gmail.com>
2014-10-14 01:29:25 +0200

    My solution

--- src/main/java/pl/allegro/jdd/StructureDiff.java
+++ src/main/java/pl/allegro/jdd/StructureDiff.java
@@ -1,13 +1,39 @@
 package pl.allegro.jdd;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.TreeTraverser;
 
-public class StructureDiff {
-    public Changes calculate(Employee oldCTO, Employee newCTO){
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Maps.difference;
+
+public class StructureDiff extends TreeTraverser<Employee> {
+    public Changes calculate(Employee oldCTO, Employee newCTO) {
         checkNotNull(oldCTO);
         checkNotNull(newCTO);
+        MapDifference<Employee, Integer> diff = difference(getSalariesMap(oldCTO), getSalariesMap(newCTO));
 
-        // your code goes here
-        throw new UnsupportedOperationException("Please implement me");
+        return new Changes(
+                diff.entriesOnlyOnLeft().keySet(),
+                diff.entriesOnlyOnRight().keySet(),
+                diff.entriesDiffering().keySet()
+        );
+    }
+
+    private Map<Employee, Integer> getSalariesMap(Employee boss) {
+        return preOrderTraversal(boss).toMap(Employee::getSalary);
+    }
+
+    /*
+    Note: if guava's TreeTraverser was a functional interface with default methods
+    rather than an abstract class, it would be possible to construct it in a more
+    simple way, asigning from a method reference that gets children of a node:
+
+    TreeTraverser<Employee> traverser = Employee::getSubordinates;
+     */
+    @Override
+    public Iterable<Employee> children(Employee node) {
+        return node.getSubordinates();
     }
 }
